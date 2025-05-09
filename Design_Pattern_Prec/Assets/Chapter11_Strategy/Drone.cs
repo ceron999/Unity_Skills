4b0efc2d6afc7b0a064db23824341119e7ec8984
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-05-09 16:58:46 +0900

    250509/ 챕터 11 구현중

---
+++ Design_Pattern_Prec/Assets/Chapter11_Strategy/Drone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.Strategy
+{
+    public class Drone : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
