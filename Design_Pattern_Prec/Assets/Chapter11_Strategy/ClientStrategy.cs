df65ce77ee95209d2e2666dd93282a5b396aef5c
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-05-09 22:42:52 +0900

    250509/ 전락 패턴 종료

---
+++ Design_Pattern_Prec/Assets/Chapter11_Strategy/ClientStrategy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Chapter.Strategy
+{
+    internal class ClientStrategy: MonoBehaviour
+    {
+        private GameObject _drone;
+        private List<IManeuverBehavior> _components = new List<IManeuverBehavior>();
+
+        private void SpawnDrone()
+        {
+            _drone = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            _drone.AddComponent<Drone>();
+
+            _drone.transform.position = UnityEngine.Random.insideUnitSphere * 10f;
+
+            ApplyRandomStrategies();
+        }
+
+        private void ApplyRandomStrategies()
+        {
+            _components.Add(_drone.AddComponent<WeavingManeuver>());
+            _components.Add(_drone.AddComponent<BoppingManeuver>());
+            _components.Add(_drone.AddComponent<FallbackManeuver>());
+
+            int index = UnityEngine.Random.Range(0, _components.Count);
+
+            _drone.GetComponent<Drone>().ApplyStrategy(_components[index]); 
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Spawn Drone"))
+                SpawnDrone();
+        }
+    }
+}
